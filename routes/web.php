75178dda4e3244b2a13be7b37096410718d695e1
JKJapona <j.japona.558573@umindanao.edu.ph>
2026-03-14 01:37:44 +0800

    Feat: Implemented CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostController;
+Route::resource('posts', PostController::class);
+
+
